ae7a9af647df90fbf9de9c00a67cb04183a41344
bridgwoodr@northshoreschools.org <you@example.com>
2023-01-24 16:14:39 -0500

    robot fram measured

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -80,7 +80,7 @@ public final class Constants {
 
         /* Swerve Profiling Values */
         /** Meters per Second */
-        public static final double maxSpeed = 4.11; //TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 4.00; //TODO: This must be tuned to specific robot
         /** Radians per Second */
         public static final double maxAngularVelocity = 10.0; //TODO: This must be tuned to specific robot
 
